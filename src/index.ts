f688b373ebd9c175f155f6b49c8ec060000c0ac7
yuki-takara <tkra.yuuki@gmail.com>
2021-04-18 08:32:21 +0900

    feat: add GfColor class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`
+type RgbType = {
+  r: number
+  g: number
+  b: number
 }
 
-console.log(hello('world'))
+const gf = ({ r, g, b }: RgbType): GfColor => {
+  return new GfColor({ r, g, b })
+}
+
+export class GfColor {
+  private readonly r: number
+  private readonly g: number
+  private readonly b: number
+
+  constructor({ r, g, b }: RgbType) {
+    this.r = r
+    this.g = g
+    this.b = b
+  }
+}
+
+export default gf
