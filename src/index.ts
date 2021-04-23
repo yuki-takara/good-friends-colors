fcaa0b321297000ca11769b74af90f272b7e8cc6
yuki-takara <tkra.yuuki@gmail.com>
2021-04-24 07:59:36 +0900

    feat: add constructor validation

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,15 @@ export class GfColor {
   private readonly b: number
 
   constructor({ r, g, b }: RgbType) {
+    if (r < 0 || 255 < r || g < 0 || 255 < g || b < 0 || 255 < b) {
+      console.error('value must be between 0 and 255.')
+      console.warn('and force to { r: 0, g: 0, b: 0 }')
+      this.r = 0
+      this.g = 0
+      this.b = 0
+      return
+    }
+
     this.r = r
     this.g = g
     this.b = b
