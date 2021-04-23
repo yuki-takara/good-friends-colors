41fd929f0e7485d3b5db1e86abf7318205340368
yuki-takara <tkra.yuuki@gmail.com>
2021-04-24 07:59:56 +0900

    feat: add toXyz

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ type RgbType = {
   b: number
 }
 
+export type XyzType = {
+  x: number
+  y: number
+  z: number
+}
+
 const gf = ({ r, g, b }: RgbType): GfColor => {
   return new GfColor({ r, g, b })
 }
@@ -26,6 +32,30 @@ export class GfColor {
     this.r = r
     this.g = g
     this.b = b
+  }
+
+  toXyz(): XyzType {
+    const srgbR = this.r / 255,
+      srgbG = this.g / 255,
+      srgbB = this.b / 255
+
+    const toLinear = (v: number): number => {
+      return v > 0.04045 ? Math.pow((v + 0.055) / 1.055, 2.4) : v / 12.92
+    }
+
+    const linearR = toLinear(srgbR),
+      linearG = toLinear(srgbG),
+      linearB = toLinear(srgbB)
+
+    const upR = linearR * 100,
+      upG = linearG * 100,
+      upB = linearB * 100
+
+    const x = upR * 0.4124 + upG * 0.3576 + upB * 0.1805
+    const y = upR * 0.2126 + upG * 0.7152 + upB * 0.0722
+    const z = upR * 0.0193 + upG * 0.1192 + upB * 0.9505
+
+    return { x, y, z }
   }
 }
 
