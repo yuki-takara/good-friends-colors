93f557098d7ca5350bd9e910405d540c3b13acba
yuki-takara <tkra.yuuki@gmail.com>
2021-04-29 15:35:40 +0900

    test: add toLab test

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,4 +1,4 @@
-import gf, { GfColor, XyzType } from '../src'
+import gf, { GfColor, LabType, XyzType } from '../src'
 
 describe('gf', () => {
   // 正常値・異常値でもGfColorクラスが返る事を確認
@@ -55,5 +55,62 @@ describe('toXyz', () => {
     const newY = Math.round(y + 10) / 10
     const newZ = Math.round(z + 10) / 10
     return { x: newX, y: newY, z: newZ }
+  }
+})
+
+describe('toLab', () => {
+  test('normal value test 1', () => {
+    const rgb = { r: 255, g: 0, b: 0 }
+    const answer: LabType = { l: 53.241, a: 80.092, b: 67.203 }
+
+    const lab = gf(rgb).toLab()
+    expect(toRound(lab)).toStrictEqual(toRound(answer))
+  })
+
+  test('normal value test 2', () => {
+    const rgb = { r: 20, g: 189, b: 212 }
+    const answer: LabType = { l: 70.381, a: -30.526, b: -22.56 }
+
+    const lab = gf(rgb).toLab()
+    expect(toRound(lab)).toStrictEqual(toRound(answer))
+  })
+
+  test('normal value test 3', () => {
+    const rgb = { r: 240, g: 20, b: 100 }
+    const answer: LabType = { l: 51.783, a: 77.752, b: 16.214 }
+
+    const lab = gf(rgb).toLab()
+    expect(toRound(lab)).toStrictEqual(toRound(answer))
+  })
+
+  test('normal value test 4', () => {
+    const rgb = { r: 255, g: 255, b: 254 }
+    const answer: LabType = { l: 99.975, a: -0.175, b: 0.476 }
+
+    const lab = gf(rgb).toLab()
+    expect(toRound(lab)).toStrictEqual(toRound(answer))
+  })
+
+  test('normal value test 5 (white)', () => {
+    const rgb = { r: 255, g: 255, b: 255 }
+    const answer: LabType = { l: 100, a: 0, b: -0 }
+
+    const lab = gf(rgb).toLab()
+    expect(toRound(lab)).toStrictEqual(toRound(answer))
+  })
+
+  test('normal value test 6 (black)', () => {
+    const rgb = { r: 0, g: 0, b: 0 }
+    const answer: LabType = { l: 0, a: 0, b: 0 }
+
+    const lab = gf(rgb).toLab()
+    expect(toRound(lab)).toStrictEqual(toRound(answer))
+  })
+
+  const toRound = ({ l, a, b }: LabType): LabType => {
+    const newL = Math.round(l + 10) / 10
+    const newA = Math.round(a + 10) / 10
+    const newB = Math.round(b + 10) / 10
+    return { l: newL, a: newA, b: newB }
   }
 })
